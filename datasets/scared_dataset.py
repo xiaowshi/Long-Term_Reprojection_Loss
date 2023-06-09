f5625b3b071184032bf883bd2c8dea2114f7d331
ItsShi <45102896+ItsShi@users.noreply.github.com>
2023-06-09 16:39:03 +0100

    Update scared_dataset.py

--- datasets/scared_dataset.py
+++ datasets/scared_dataset.py
@@ -7,7 +7,9 @@ import PIL.Image as pil
 import cv2
 
 from .mono_dataset import MonoDataset
-
+import torchvision.transforms as transforms
+from torch.utils.data import Dataset
+from PIL import Image  
 
 class SCAREDDataset(MonoDataset):
     def __init__(self, *args, **kwargs):
@@ -46,7 +48,10 @@ class SCAREDRAWDataset(SCAREDDataset):
 #         image_path = os.path.join(
 #             self.data_path, folder, "0{}".format(self.side_map[side]), f_str)
         image_path = os.path.join(
-            self.data_path, "0{}".format(self.side_map[side]), folder, f_str)
+            self.data_path, 
+            # "0{}".format(self.side_map[side]), 
+            # folder, 
+            f_str)
         return image_path
 
     def get_depth(self, folder, frame_index, side, do_flip):
@@ -65,3 +70,51 @@ class SCAREDRAWDataset(SCAREDDataset):
             depth_gt = np.fliplr(depth_gt)
 
         return depth_gt
+
+
+         
+def pil_loader(path):
+    # open path as file to avoid ResourceWarning
+    # (https://github.com/python-pillow/Pillow/issues/835)
+    with open(path, 'rb') as f:
+        with Image.open(f) as img:
+            return img.convert('RGB')
+class SCAREDNAIVEDataset(Dataset):
+    def __init__(self, data_path, filenames, transform=None):
+        self.filenames = filenames
+        self.data_path = data_path
+        self.img_ext = ".jpg"
+        self.loader = pil_loader
+        self.transform = transform
+
+    def __len__(self):
+        return len(self.filenames)
+
+    def __getitem__(self, index):
+        inputs = {}
+        line = self.filenames[index].split()
+        folder = line[0]
+
+        if len(line) == 3:
+            frame_index = int(line[1])
+        else:
+            frame_index = 0
+
+        if len(line) == 3:
+            side = line[2]
+        else:
+            side = None
+        # get image path
+        f_str = "{:06d}{}".format(frame_index, self.img_ext)
+        image_path = os.path.join(
+            self.data_path,
+            # "02",
+            # folder[0] + folder[7] + folder[9] + folder[-1],
+            f_str)
+        inputs[("color", 0, 0)] = self.loader(image_path)
+        # inputs[("color", 0, 0)] = Image.open(image_path).convert('RGB')
+
+        if self.transform:
+            inputs[("color", 0, 0)] = self.transform(inputs[("color", 0, 0)])
+        return inputs
+
