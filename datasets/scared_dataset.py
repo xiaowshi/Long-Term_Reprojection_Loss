c4793c93de2bd8eee15f1e534092030ce174f2c2
ItsShi <45102896+ItsShi@users.noreply.github.com>
2023-06-10 15:09:54 +0100

    Update scared_dataset.py

--- datasets/scared_dataset.py
+++ datasets/scared_dataset.py
@@ -7,8 +7,10 @@ import PIL.Image as pil
 import cv2
 
 from .mono_dataset import MonoDataset
+import torchvision.transforms as transforms
 from torch.utils.data import Dataset
-from PIL import Image
+from PIL import Image  
+import random
 
 class SCAREDDataset(MonoDataset):
     def __init__(self, *args, **kwargs):
@@ -40,11 +42,13 @@ class SCAREDRAWDataset(SCAREDDataset):
         super(SCAREDRAWDataset, self).__init__(*args, **kwargs)
 
     def get_image_path(self, folder, frame_index, side):
+
         f_str = "{:06d}{}".format(frame_index, self.img_ext)
         image_path = os.path.join(
-            self.data_path,
-            "02",
-            folder[0] + folder[7] + folder[9] + folder[-1],
+            self.data_path, 
+            # "02",
+            # folder,
+            # folder[0] + folder[7] + folder[9] + folder[-1],
             f_str)
         return image_path
 
@@ -55,7 +59,6 @@ class SCAREDRAWDataset(SCAREDDataset):
             self.data_path,
             "01",
             folder,
-            "image_0{}/data/groundtruth".format(self.side_map[side]),
             f_str)
 
         depth_gt = cv2.imread(depth_path, 3)
@@ -67,15 +70,56 @@ class SCAREDRAWDataset(SCAREDDataset):
         return depth_gt
 
 
+         
+def pil_loader(path):
+    # open path as file to avoid ResourceWarning
+    # (https://github.com/python-pillow/Pillow/issues/835)
+    with open(path, 'rb') as f:
+        with Image.open(f) as img:
+            return img.convert('RGB')
+            
 class SCAREDNAIVEDataset(Dataset):
-    def __init__(self, data_path, filenames,):
+    # def __init__(self, *args, **kwargs):
+    def __init__(self, data_path, filenames, transform=None, img_ext='.jpg', is_train=False):
+        # , height, width, frame_ids, num_scales, is_train=True, img_ext='.jpg', transform=None):
         self.filenames = filenames
         self.data_path = data_path
+        self.img_ext = img_ext
+        self.loader = pil_loader
+        self.transform = transform
+        self.is_train = is_train
 
-    def __getitem__(self, index):
+    def __len__(self):
+        return len(self.filenames)
+
+    def preprocess(self, inputs, color_aug):
+        """Resize colour images to the required scales and augment if required
+
+        We create the color_aug object in advance and apply the same augmentation to all
+        images in this item. This ensures that all images input to the pose network receive the
+        same augmentation.
+        """
+        # for k in list(inputs):
+        #     frame = inputs[k]
+        #     if "color" in k:
+        #         # print(k)
+        #         n, im, i = k # 
+        #         for i in range(4):# num_scales
+        #             print("resize:", k, (n, im, i - 1))
+        #             inputs[(n, im, i)] = self.resize[i](inputs[(n, im, i - 1)])
+
+        for k in list(inputs):
+            f = inputs[k]
+            if "color" in k:
+                n, im, i = k
+                print("tensor:", k)
+                inputs[(n, im, i)] = self.to_tensor(f)
+                inputs[(n + "_aug", im, i)] = self.to_tensor(color_aug(f))
+
+    def get_iamge_path(self, index):
+        # get image path
         line = self.filenames[index].split()
         folder = line[0]
-        inputs = {}
 
         if len(line) == 3:
             frame_index = int(line[1])
@@ -86,40 +130,43 @@ class SCAREDNAIVEDataset(Dataset):
             side = line[2]
         else:
             side = None
-        # get image path
         f_str = "{:06d}{}".format(frame_index, self.img_ext)
         image_path = os.path.join(
             self.data_path,
-            "02",
-            folder[0] + folder[7] + folder[9] + folder[-1],
+            # "02",
+            # folder[0] + folder[7] + folder[9] + folder[-1],
             f_str)
-        inputs[("color", 0, 0)] = self.loader(image_path)
-        return inputs
-
-    # def get_image_path(self, folder, frame_index, side):
-    #     f_str = "{:06d}{}".format(frame_index, self.img_ext)
-    #     image_path = os.path.join(
-    #         self.data_path,
-    #         "02",
-    #         folder[0] + folder[7] + folder[9] + folder[-1],
-    #         f_str)
-    #     return image_path
+        return image_path
     
-    # def get_depth(self, folder, frame_index, side, do_flip):
-    #     f_str = "scene_points{:06d}.tiff".format(frame_index-1)
+    def __getitem__(self, index):
+        inputs = {}
+        self.resize = {}
+        self.to_tensor = transforms.ToTensor()
 
-    #     depth_path = os.path.join(
-    #         self.data_path,
-    #         "01",
-    #         folder,
-    #         "image_0{}/data/groundtruth".format(self.side_map[side]),
-    #         f_str)
+        image_path = self.get_iamge_path(index)
+        inputs[("color", 0, 0)] = self.loader(image_path)
 
-    #     depth_gt = cv2.imread(depth_path, 3)
-    #     depth_gt = depth_gt[:, :, 0]
-    #     depth_gt = depth_gt[0:1024, :]
-    #     if do_flip:
-    #         depth_gt = np.fliplr(depth_gt)
+        do_color_aug = self.is_train and random.random() > 0.5
+        if do_color_aug:
+            color_aug = transforms.ColorJitter(
+                (0.8, 1.2), (0.8, 1.2), (0.8, 1.2), (-0.1, 0.1))
+                # self.brightness, self.contrast, self.saturation, self.hue)
+        else:
+            color_aug = (lambda x: x)
+        
+        # for i in [0, -1, 1]: # self.frame_idxs:
+        #     if i == "s":
+        #         other_side = {"r": "l", "l": "r"}[side]
+        #         inputs[("color", i, -1)] = self.get_color(folder, frame_index, other_side, do_flip)
+        #     else:
+        #         inputs[("color", i, -1)] = self.get_color(folder, frame_index + i, side, do_flip)
 
-    #     return depth_gt
+        self.preprocess(inputs, color_aug)
+        # for i in [0, -1, 1]:# self.frame_idxs:
+        #     del inputs[("color", i, -1)]
+        #     del inputs[("color_aug", i, -1)]
 
+        if self.transform:
+            inputs[("color", 0, 0)] = self.transform(inputs[("color", 0, 0)])
+        print("asdfsadf", inputs.keys())
+        return inputs
