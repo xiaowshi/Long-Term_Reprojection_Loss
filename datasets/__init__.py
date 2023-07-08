5867bc08c5da558e0d6e2db6ad3a87d440c2bafe
ItsShi <45102896+ItsShi@users.noreply.github.com>
2023-07-08 14:09:58 +0100

    Update __init__.py

--- datasets/__init__.py
+++ datasets/__init__.py
@@ -1,3 +1,3 @@
-from .kitti_dataset import KITTIRAWDataset, KITTIOdomDataset, KITTIDepthDataset
+# from .kitti_dataset import KITTIRAWDataset, KITTIOdomDataset, KITTIDepthDataset
 from .scared_dataset import SCAREDRAWDataset
 from .scared_dataset import SCAREDNAIVEDataset
